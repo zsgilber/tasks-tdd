508a42f21fcb59577ae05183c62ad7838d7eeb30
Zach Gilbert <zsgilber@gmail.com>
2018-05-20 20:32:52 -0700

    change test to fail if not 404

--- app-test.py
+++ app-test.py
@@ -2,13 +2,14 @@ from app import app
 
 import unittest
 
+
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 404)
+
 
 if __name__ == '__main__':
     unittest.main()
